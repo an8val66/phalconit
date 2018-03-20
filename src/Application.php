48c90a3bdb2d9bd38cdc004f074c2d74bb4b02a3
Anderson Borges <andersonvaleriano.borges@gmail.com>
2018-03-20 13:38:38 -0300

    added loaders

--- src/Application.php
+++ src/Application.php
@@ -12,13 +12,18 @@ class Application extends \Phalcon\Mvc\Application
     /**
      * @var string
      */
-    protected $env;
+    protected $_env;
 
-    public function __construct($env, \Phalcon\DiInterface $di = null)
+    /**
+     * @var array
+     */
+    protected $_configuration;
+
+    public function __construct($env, array $configuration, \Phalcon\DiInterface $di = null)
     {
-        $this->env = $env;
+        $this->_env = $env;
 
-        switch ($this->env) {
+        switch ($this->getEnv()) {
             case self::ENV_DIST:
                 ini_set('display_errors', 0);
                 ini_set('display_startup_errors', 0);
@@ -34,10 +39,66 @@ class Application extends \Phalcon\Mvc\Application
                 throw new \Exception('Wrong environment variable $env passed: '. $env);
         }
 
+        $this->_configuration = $configuration;
+
         if (is_null($di)) {
             $di = new \Phalcon\Di\FactoryDefault();
         }
 
         parent::__construct($di);
+    }
+
+    public function registerLoader()
+    {
+        $config = $this->getConfiguration();
+
+        $loader = new \Phalcon\Loader();
+
+        if (isset($config['application']['registerNamespaces'])) {
+            $loadedNamespaces = $config['application']['registerNamespaces'];
+        } else {
+            $loadedNamespaces = [];
+        }
+
+        foreach ($config['application']['modules'] as $module) {
+            $loadedNamespaces[$module . '\Models'] = $config['paths']['modulesDir'] . $module . '/models';
+        }
+
+        if (isset($config['application']['registerDirs'])) {
+            $loader->registerDirs($config['application']['registerDirs']);
+        }
+
+        $loader->registerNamespaces($loadedNamespaces)->register();
+    }
+
+    public function bootstrap()
+    {
+        $this->registerLoader();
+    }
+
+    public function run($uri = null)
+    {
+        $this->handle($uri)
+            ->send();
+    }
+
+    /**
+     * Get environment
+     *
+     * @return string
+     */
+    public function getEnv()
+    {
+        return $this->_env;
+    }
+
+    /**
+     * Get configuration
+     *
+     * @return array
+     */
+    public function getConfiguration()
+    {
+        return $this->_configuration;
     }
 }
